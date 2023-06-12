f0c65c6795e30e2a0c384f720c9c04d1eb15afce
ABHIJITRAUT123 <abhijitraut150@gmail.com>
2023-06-12 20:28:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './Component/Footer';
 import Nav from './Component/Nav';
 
 function App() {
   return (
     <>
     <Nav />
+    <Footer />
     </>
   );
 }
---
+++ src/Component/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <>
+        <h1>footer fitness</h1>
+        </>
+    )
+}
