48c048821e3d99ea889ba1816bcf232e447aa2b2
ABHIJITRAUT123 <abhijitraut150@gmail.com>
2023-06-13 15:02:24 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import styled from 'styled-components';
 import './App.css';
 import Footer from './Component/Footer';
 import Nav from './Component/Nav';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Component/Home';
+import AboutUs from './Component/AboutUs';
+import OurServices from './Component/OurServices';
+import ContactUs from './Component/ContactUs';
+
+var Main = styled.section`
+`
+var PageNotFound = styled.h1`
+color: white;`
 
 function App() {
   return (
-    <>
-    <Nav />
-    <Footer />
-    </>
+    <Main>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/our-services' element={<OurServices />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='*' element={<PageNotFound className='text-center mt-5 mb-5'>Page Not Found</PageNotFound>} />
+      </Routes>
+      <Footer />
+    </Main>
   );
 }
 
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 
-export default function Footer(){
+export default function Home(){
     return(
         <section className="container">
             
